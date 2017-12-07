75736861f9fb1acf4dcfb1591dd399a4bbae3264
csongor666 <32299927+csongor666@users.noreply.github.com>
2017-12-07 12:07:40 +0100

    Add files via upload

---
+++ eagle_tree_GPS.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySer(3, 4); //digital 3 digital 4 pin
+
+char inData = '0';
+
+void setup(){
+     Serial.begin(38400); 
+      mySer.begin(38400);
+}
+
+void loop(){
+    if(mySer.available()){
+     inData = mySer.read(); 
+     Serial.print(inData);
+  }
+}
